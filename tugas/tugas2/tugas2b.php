4c2317ce8f9d481a9a63ebf3e020b5ead8f492e9
syaaugiii <syawgy@gmail.com>
2025-03-21 08:39:24 +0700

    tugas2

---
+++ tugas/tugas2/tugas2b.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- cara pertama -->
+        <!-- <br> <?php 
+        for ($i = 1; $i <= 1; $i++) {
+                echo "$i";
+        } 
+        ?>
+        <br> <?php 
+        for ($i = 1; $i <= 2; $i++) {
+                echo "$i";
+        } 
+        ?>  <br> <?php 
+        for ($i = 1; $i <= 3; $i++) {
+            echo "$i";
+        } 
+        ?>  <br> <?php 
+        for ($i = 1; $i <= 4; $i++) {
+            echo "$i";
+        } 
+        ?>  <br> <?php 
+            for ($i = 1; $i <= 5; $i++) {
+            echo "$i";
+        } 
+        ?>  <br> <?php 
+        for ($i = 1; $i <= 6; $i++) {
+                echo "$i";
+        } 
+        ?>  <br> <?php 
+        for ($i = 1; $i <= 7; $i++) {
+            echo "$i";
+        } 
+        ?>  <br> <?php 
+        for ($i = 1; $i <= 8; $i++) {
+            echo "$i";
+        } 
+        ?>  <br> <?php 
+        for ($i = 1; $i <= 9; $i++) {
+            echo "$i";
+        } 
+        ?> 
+        <br> <?php 
+        for ($i = 1; $i <= 10; $i++) {
+             echo "$i";
+        } 
+        ?> -->
+        <!-- cara kedua -->
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+            for ($j = 1; $j <= $i; $j++) {
+                echo $j ." ";
+            }
+            echo "<br>";
+       } 
+        ?>
+            
+</body>
+</html>
