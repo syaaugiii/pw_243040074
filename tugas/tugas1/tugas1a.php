cc84846a9a1a3f4ecb3bc0f641dda1cdd13fee73
syaaugiii <syawgy@gmail.com>
2025-03-21 07:31:38 +0700

    tugas1

---
+++ tugas/tugas1/tugas1a.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = "Topi";
+        $b = "Bundar";
+
+        echo "<i><h2>''$a saya $b, $b $a saya''</h2></i>";
+    ?>
+</body>
+</html>
---
+++ tugas/tugas1/tugas1c.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+.kotak {
+    height: 80px;
+    width: 80px;
+    background-color: salmon;
+    text-align: center;
+    line-height: 5;
+    border: solid 2px black;
+}
+
+.baris2 {
+    display: flex;
+}
+
+.baris3 {
+    display: flex;
+}
+</style>
+<body>
+    <div class="baris1">
+         <div class="kotak">1</div>
+    </div>
+    <div class="baris2">
+        <div class="kotak">2</div>
+        <div class="kotak">2</div>
+    </div>
+    <div class="baris3">
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+        <div class="kotak">3</div>
+    </div>
+</body>
+</html>
